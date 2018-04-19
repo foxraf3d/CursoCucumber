a3df666d086b93c3c7b1fcc2bd02f628607a9caf
rafaelsilva <foxraf3d@gmail.com>
2018-04-19 17:42:02 -0300

    Curso de BDD com cucumber

---
+++ CursoCucumber/src/test/java/aprender_cucumberSteps.java
@@ -0,0 +1,24 @@
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
+import sun.security.util.PendingException;
+
+public class aprender_cucumberSteps {
+
+    @Given("^que criei o arquivo corretamente$")
+    public void que_criei_o_arquivo_corretamente() throws Throwable {
+
+    }
+
+    @When("^executá-lo$")
+    public void executá_lo() throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+
+    @Then("^a especificação deve finalizar com sucesso$")
+    public void a_especificação_deve_finalizar_com_sucesso() throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+}
